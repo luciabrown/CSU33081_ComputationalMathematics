206abcad8ed1a5a148955887a13b90cd5ba63afd
luciabrown <brownlu@tcd.ie>
2024-10-01 21:10:51 +0100

    Assignment 1 Question 1 specification

---
+++ Question1.m
@@ -0,0 +1,9 @@
+% Determining the square root of a number p is the same as finding the root of the equation f(x) = x^2 - p = 0.
+
+% Write a MATLAB user defined function that determines the square root of a positive number by solving this equation using the NewtonRaphson Method.
+% Name the function Xs = SquareRoot(p).
+% The output argument Xs is the answer and p is the number whose square root is to be determined.
+% The starting value for x should be x = p
+% and the iterations should stop when the estimated relative error is less than 0.00001. 
+% The maximum number of iterations should be 20.
+% After 5 iterations the program calculates the square root of 729 to be (rounded to two decimal places):
