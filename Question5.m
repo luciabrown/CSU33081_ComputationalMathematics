1c74f6542920a8e166f4565b82f32528a15efc16
luciabrown <brownlu@tcd.ie>
2024-10-02 13:36:19 +0100

    Question 5 spec

---
+++ Question5.m
@@ -0,0 +1,4 @@
+% Use the Secant Method to find a root of the function
+% f(x) = 16x^5 - 73x^2 - 133
+% accurrate to an error of e = xn - xn-1 = 0.001 where xn is the value of x at the nth iteration
+% Use starting points x0=3 and x1 = 2.5
