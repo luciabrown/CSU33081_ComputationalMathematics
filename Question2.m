a8321f936af121e60ae09d0963998098528b6a57
luciabrown <brownlu@tcd.ie>
2024-10-11 15:21:55 +0100

    Question 2 commented

--- Question2.m
+++ Question2.m
@@ -2,23 +2,23 @@
 
 function x = GaussJordan(a,b)
     
-    [n,m] = size(a);
-    if n~=m
-        error('Matrix must be square');
+    [n,m] = size(a);                    % If A's rows != A's columns
+    if n~=m                             
+        error('Matrix must be square'); % GaussJordan cannot work
     end
 
-    ab=[a,b];
-    [R,C] = size(ab);
+    ab=[a,b];                           % Augment A's matrix with B solution vector
+    [R,C] = size(ab);                   % AB matrix's rows and columns
 
-    for j=1:R-1
+    for j=1:R-1                         % Iterate over the matrix
 
     % Pivoting section starts
-        if ab(j,j) ==0
-            for k = j + 1:R
-                if ab(k,j) ~=0
-                    abTemp = ab(j,:);
-                    ab(j,:) = ab(k,:);
-                    ab(k,:) = abTemp;
+        if ab(j,j) ==0                  % Check if pivot element is zero
+            for k = j + 1:R             % Swap current row with a row containing non-zero element
+                if ab(k,j) ~=0          % If another row's element !=0
+                    abTemp = ab(j,:);   
+                    ab(j,:) = ab(k,:);  % Pivot row = other row
+                    ab(k,:) = abTemp;   % Other row = pivot row
                     break
                 end
             end
@@ -26,13 +26,13 @@ function x = GaussJordan(a,b)
     % Pivoting section ends
 
         % Normalise the pivot row / make pivot element 1
-        ab(j,:) = ab(j,:) - ab(j,j);
+        ab(j,:) = ab(j,:) - ab(j,j);                    % Divide entire row by pivot element
 
         % Eliminate other rows
-        for i = 1:R
-            if i~= j
-                ab(i,:) = ab(i,:) - ab(i,j)*ab(j,:);
-            end
+        for i = 1:R                                 
+            if i~= j                                    % For all rows except pivot row
+                ab(i,:) = ab(i,:) - ab(i,j)*ab(j,:);    % Subtract pivot row from current row
+            end    
         end
     end
 
